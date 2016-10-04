8f1526a21aaf8c9161efa312b312a78df66f49a2
Matthew Jeorrett <matthewjeorrett@gmail.com>
2016-10-04 09:59:03 +0100

    changed tests to use constants in setup

--- specs/car_spec.rb
+++ specs/car_spec.rb
@@ -5,38 +5,40 @@ require('pry-byebug')
 
 class CarTest < MiniTest::Test
 
+  CAR1_MAKE = "Honda"
+  CAR1_MODEL = "Accord"
+  CAR1_BRAKE_POWER = 9
+  CAR1_MAX_FUEL = 100
+
+  ENGINE1_ACCELERATION = 10
+  ENGINE1_FUEL_USAGE = 5
+  ENGINE1_MAX_SPEED = 123
+
   def setup()
-    @engine1_acceleration = 10
-    @engine1_fuel_usage = 5
-    @engine1_max_speed = 151
 
     @engine1 = Engine.new(
-      @engine1_acceleration,
-      @engine1_fuel_usage,
-      @engine1_max_speed
+      ENGINE1_ACCELERATION,
+      ENGINE1_FUEL_USAGE,
+      ENGINE1_MAX_SPEED
     )
 
-    @car1_make = "Honda"
-    @car1_model = "Accord"
-    @car1_brake_power = 9
-    @car1_max_fuel = 100
-    @car1 = Car.new(@car1_make, @car1_model, @engine1, @car1_brake_power, @car1_max_fuel)
+    @car1 = Car.new(CAR1_MAKE, CAR1_MODEL, @engine1, CAR1_BRAKE_POWER, CAR1_MAX_FUEL)
   end
 
   def test_make()
-    assert_equal(@car1_make, @car1.make)
+    assert_equal(CAR1_MAKE, @car1.make)
   end
 
   def test_model()
-    assert_equal(@car1_model, @car1.model)
+    assert_equal(CAR1_MODEL, @car1.model)
   end
 
   def test_brake_power()
-    assert_equal(@car1_brake_power, @car1.brake_power)
+    assert_equal(CAR1_BRAKE_POWER, @car1.brake_power)
   end
 
   def test_max_fuel()
-    assert_equal(@car1_max_fuel, @car1.max_fuel)
+    assert_equal(CAR1_MAX_FUEL, @car1.max_fuel)
   end
 
   def test_initial_fuel_level()
@@ -48,7 +50,7 @@ class CarTest < MiniTest::Test
   end
 
   def test_run_out_of_fuel()
-    number_of_accelerations = (@car1_max_fuel / @engine1_fuel_usage).floor
+    number_of_accelerations = (CAR1_MAX_FUEL / ENGINE1_FUEL_USAGE).floor
     number_of_accelerations.times do
       @car1.accelerate
     end
@@ -58,42 +60,42 @@ class CarTest < MiniTest::Test
 
   def test_accelerate()
     @car1.accelerate
-    expected_speed = @engine1_acceleration
-    expected_fuel = @car1_max_fuel - @engine1_fuel_usage
+    expected_speed = ENGINE1_ACCELERATION
+    expected_fuel = CAR1_MAX_FUEL - ENGINE1_FUEL_USAGE
     assert_equal(expected_speed, @car1.speed)
     assert_equal(expected_fuel, @car1.fuel_level)
   end
 
   def test_accelerate_through_max_speed()
     #accelerate till we are less than one more acceleration away from top speed.
-    until @car1.speed >= @engine1_max_speed
+    until @car1.speed >= ENGINE1_MAX_SPEED
       @car1.accelerate()
     end
 
     @car1.accelerate()
 
-    assert_equal(@engine1_max_speed, @car1.speed)
+    assert_equal(ENGINE1_MAX_SPEED, @car1.speed)
   end
 
   def test_refuel()
-    while @car1.fuel_level >= @engine1_fuel_usage
+    while @car1.fuel_level >= ENGINE1_FUEL_USAGE
       @car1.accelerate
     end
     @car1.refuel
-    assert_equal(@car1_max_fuel, @car1.fuel_level)
+    assert_equal(CAR1_MAX_FUEL, @car1.fuel_level)
   end
 
   def test_dont_brake_below_zero()
     @car1.brake
     assert_equal(0, @car1.speed)
-    assert_equal(@car1_max_fuel, @car1.fuel_level)
+    assert_equal(CAR1_MAX_FUEL, @car1.fuel_level)
   end
 
   def test_brake_when_moving()
     @car1.accelerate
     @car1.accelerate
     @car1.brake
-    expected_speed = (@engine1_acceleration * 2) - @car1_brake_power
+    expected_speed = (ENGINE1_ACCELERATION * 2) - CAR1_BRAKE_POWER
     assert_equal(expected_speed, @car1.speed)
   end
 
--- specs/engine_spec.rb
+++ specs/engine_spec.rb
@@ -5,43 +5,44 @@ require('pry-byebug')
 
 class EngineTest < MiniTest::Test
 
+  ENGINE1_ACCELERATION = 10
+  ENGINE1_FUEL_USAGE = 5
+  ENGINE1_MAX_SPEED = 151
+
   def setup
-    @engine1_acceleration = 10
-    @engine1_fuel_usage = 5
-    @engine1_max_speed = 151
 
     @engine1 = Engine.new(
-      @engine1_acceleration,
-      @engine1_fuel_usage,
-      @engine1_max_speed
+      ENGINE1_ACCELERATION,
+      ENGINE1_FUEL_USAGE,
+      ENGINE1_MAX_SPEED
     )
   end
 
   def test_acceleration()
-    assert_equal(@engine1_acceleration, @engine1.acceleration)
+    assert_equal(ENGINE1_ACCELERATION, @engine1.acceleration)
   end
 
   def test_fuel_usage()
-    assert_equal(@engine1_fuel_usage, @engine1.fuel_usage)
+    assert_equal(ENGINE1_FUEL_USAGE, @engine1.fuel_usage)
   end
 
   def test_max_speed()
-    assert_equal(@engine1_max_speed, @engine1.max_speed)
+    assert_equal(ENGINE1_MAX_SPEED, @engine1.max_speed)
   end
 
   def test_accelerate()
     start_speed = 15
-    expected_speed = start_speed + @engine1_acceleration
+    expected_speed = start_speed + ENGINE1_ACCELERATION
     assert_equal(expected_speed, @engine1.accelerate(start_speed))
   end
 
   def test_accelerate_through_max_speed()
-    actual = @engine1.accelerate(@engine1_max_speed - 2)
-    assert_equal(@engine1_max_speed, actual)
+    actual = @engine1.accelerate(ENGINE1_MAX_SPEED - 2)
+    assert_equal(ENGINE1_MAX_SPEED, actual)
   end
 
   def test_accelerate_from_max_speed()
-    actual = @engine1.accelerate(@engine1_max_speed)
-    assert_equal(@engine1_max_speed, actual)
+    actual = @engine1.accelerate(ENGINE1_MAX_SPEED)
+    assert_equal(ENGINE1_MAX_SPEED, actual)
   end
 end
